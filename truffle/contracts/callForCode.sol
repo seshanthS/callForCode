7ed34a38e72c70aed70b7ab2fe1da21bc1f338d4
seshanthS <seshanth.s@hotmail.com>
2019-03-13 22:03:17 +0530

    basic functionalities - Continued

--- truffle/contracts/callForCode.sol
+++ truffle/contracts/callForCode.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.5.0;
 
+//TODO
+//add mapping(_isssueId => mapping(_beneficiaryAddress => _amount));
+//withdraw function, beneficiary can withdraw .
 
 contract calForCode {
   constructor() public {
@@ -20,10 +23,12 @@ contract calForCode {
 
   //caseId, donaterslist
   mapping(uint=>address[])donaters;
+  mapping(uint => address[])beneficiary;//beneficiary are the shopkeeper, in the given example.
   mapping(uint=>mapping(address=>uint))private voteWeight;
   mapping(uint => uint)private amountAvailableForIssue;
   mapping(uint => uint)private expiry;
   mapping(uint=>mapping(address=>bool))voted;
+  mapping(uint => bool)expired; // issueId, bool
   //issueId, Votes;
   mapping(uint => uint )positiveVotes;
   mapping(uint => uint )negativeVotes;
@@ -32,13 +37,14 @@ contract calForCode {
   event newIssueCreated(uint _amount,uint _expiry, bytes _data);
   event IssueFunded(uint indexed _IssiuId, uint _amount);
 
-  function openIssue(uint _amount,uint _expiry, bytes memory _data)public {
+  function openIssue(uint _amount,uint _expiry, bytes memory _data, address _beneficiary)public {
     require(_amount >1);
     issueId++;
     donaters[issueId].push(msg.sender);
     //INSERT HERE : transfer DAI to account;
-    //->
+    transferDai(msg.sender,address(this), _amount);
 
+    beneficiary[issueId].push(_beneficiary);
     //Increase vote weight
     voteWeight[issueId][msg.sender] += _amount;
     amountAvailableForIssue[issueId] += _amount;
@@ -46,10 +52,14 @@ contract calForCode {
     emit newIssueCreated(_amount, expiry[issueId], _data);
   }
 
-  function fundIssue(uint _issueId,uint _amount)public {
+  function fundIssue(uint _issueId,uint _amount, address _beneficiary)public {
     require(expiry[_issueId] < now, "Expired");
     require(_amount > 1);
-    transferDai();
+    
+    beneficiary[_issueId].push(_beneficiary);
+
+    transferDai(msg.sender, address(this), _amount);
+    
     voteWeight[issueId][msg.sender] += _amount;
     amountAvailableForIssue[issueId] += _amount;
 
@@ -62,11 +72,13 @@ contract calForCode {
       _choice == true means, its a positive vote
       _choice == false, -> Negative Vote
     */
+    checkAndMarkExpired(_issueId);
 
     require(voteWeight[_issueId][msg.sender] > 1,"Only donaters Can vote");
-    require(expiry[_issueId] < now,"Expired");
+
     //Only one vote per user
     require(voted[_issueId][msg.sender] == false, "Already Voted");
+    require(expired[_issueId] == false,"Already Expired");
     
     //set voted to true
     voted[_issueId][msg.sender] = true;
@@ -76,7 +88,32 @@ contract calForCode {
     negativeVotes[_issueId] += voteWeight[_issueId][msg.sender];
   }
 
-  function results()public;
+  //returns true if positive votes is greater
+  function results(uint _issueId)public returns(bool){
+    checkAndMarkExpired(_issueId);
+    require(expired[_issueId] == true, "Deadline not reached yet...");
 
-  function transferDai()public;
+    if(positiveVotes[_issueId] > negativeVotes[_issueId]){
+      //more positive votes
+      
+      return true;
+    }else {
+      //more negative votes
+      return false;
+    }
+
+  }
+
+  function transferDai(address _from, address _to, uint _amount)public;
+
+  //return true if expired
+  function checkAndMarkExpired(uint _issueId)public returns (bool){
+    if(expiry[_issueId] < now) {
+      expired[_issueId] = true;
+      return true;
+    }else {
+      return false;
+    }
+  }
 }
+
